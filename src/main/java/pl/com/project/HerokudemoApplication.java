765ecf3e69ed3bfcaa479eda0af30bbac1cb6eec
Piotr Kwasek <kwasekpiotr98@gmail.com>
2019-04-12 00:27:33 +0200

    cors delete

--- src/main/java/pl/com/project/HerokudemoApplication.java
+++ src/main/java/pl/com/project/HerokudemoApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
@@ -24,8 +28,22 @@ public class HerokudemoApplication {
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
-			http.cors().disable();
 			http.csrf().disable();
+		}
+	}
+
+	@Configuration
+	public class MyConfiguration {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurerAdapter() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
 		}
 	}
 }

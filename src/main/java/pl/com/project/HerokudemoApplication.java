8649bca995005f54f00f5652bbf9a36662e34072
Piotr Kwasek <kwasekpiotr98@gmail.com>
2019-04-18 15:58:28 +0200

    fix

--- src/main/java/pl/com/project/HerokudemoApplication.java
+++ src/main/java/pl/com/project/HerokudemoApplication.java
@@ -10,13 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.Arrays;
 
 
 @SpringBootApplication
@@ -36,4 +32,19 @@ public class HerokudemoApplication {
 		}
 	}
 
+	@Configuration
+	public class MyConfiguration {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurerAdapter() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+
+			};
+		}
+	}
 }
--- src/main/java/pl/com/project/Notices/Notice.java
+++ src/main/java/pl/com/project/Notices/Notice.java
@@ -15,32 +15,28 @@ public class Notice {
     private StringBuffer note = new StringBuffer(500);
     private Date meeting_date = new Date();
     private Integer user_id_user;
-    private Float price;
-    private Integer subject_id_subject;
-    private char active;
-    private Date timestamp=new Date();
-    private Float time_from;
-    private Float time_to;
-    private Integer meeting_id_meeting;
-    private char look_or_offer;
-    private String subject;
 
-    public Notice(String meeting_place, StringBuffer note, Date meeting_date, Integer user_id_user, Float price,
-                  Integer subject_id_subject, char active, Date timestamp, Float time_from, Float time_to, Integer meeting_id_meeting,
-                  char look_or_offer, String subject) {
+    public Date getMeeting_date() {
+        return meeting_date;
+    }
+
+    public void setMeeting_date(Date meeting_date) {
+        this.meeting_date = meeting_date;
+    }
+
+    public Integer getUser_id_user() {
+        return user_id_user;
+    }
+
+    public void setUser_id_user(Integer user_id_user) {
+        this.user_id_user = user_id_user;
+    }
+
+    public Notice(String meeting_place, StringBuffer note, Date meeting_date, Integer user_id_user) {
         this.meeting_place = meeting_place;
         this.note = note;
         this.meeting_date = meeting_date;
         this.user_id_user = user_id_user;
-        this.price = price;
-        this.subject_id_subject = subject_id_subject;
-        this.active = active;
-        this.timestamp = timestamp;
-        this.time_from = time_from;
-        this.time_to = time_to;
-        this.meeting_id_meeting = meeting_id_meeting;
-        this.look_or_offer = look_or_offer;
-        this.subject = subject;
     }
 
     public Notice() {
@@ -68,93 +64,5 @@ public class Notice {
 
     public void setNote(StringBuffer note) {
         this.note = note;
-    }
-
-    public Date getMeeting_date() {
-        return meeting_date;
-    }
-
-    public void setMeeting_date(Date meeting_date) {
-        this.meeting_date = meeting_date;
-    }
-
-    public Integer getUser_id_user() {
-        return user_id_user;
-    }
-
-    public void setUser_id_user(Integer user_id_user) {
-        this.user_id_user = user_id_user;
-    }
-
-    public Float getPrice() {
-        return price;
-    }
-
-    public void setPrice(Float price) {
-        this.price = price;
-    }
-
-    public Integer getSubject_id_subject() {
-        return subject_id_subject;
-    }
-
-    public void setSubject_id_subject(Integer subject_id_subject) {
-        this.subject_id_subject = subject_id_subject;
-    }
-
-    public char getActive() {
-        return active;
-    }
-
-    public void setActive(char active) {
-        this.active = active;
-    }
-
-    public Date getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public Float getTime_from() {
-        return time_from;
-    }
-
-    public void setTime_from(Float time_from) {
-        this.time_from = time_from;
-    }
-
-    public Float getTime_to() {
-        return time_to;
-    }
-
-    public void setTime_to(Float time_to) {
-        this.time_to = time_to;
-    }
-
-    public Integer getMeeting_id_meeting() {
-        return meeting_id_meeting;
-    }
-
-    public void setMeeting_id_meeting(Integer meeting_id_meeting) {
-        this.meeting_id_meeting = meeting_id_meeting;
-    }
-
-    public char getLook_or_offer() {
-        return look_or_offer;
-    }
-
-    public void setLook_or_offer(char loor_or_offer) {
-        this.look_or_offer = loor_or_offer;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
     }
 }

76b8f1c49404234b999925677529c94617f73e67
krolik1337 <kamil.krol.1997@gmail.com>
2019-04-19 14:15:08 +0200

    sync with pk

--- src/main/java/pl/com/project/HerokudemoApplication.java
+++ src/main/java/pl/com/project/HerokudemoApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
@@ -25,6 +29,22 @@ public class HerokudemoApplication {
 		protected void configure(HttpSecurity http) throws Exception {
 			http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
 			http.csrf().disable();
+		}
+	}
+
+	@Configuration
+	public class MyConfiguration {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurerAdapter() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+
+			};
 		}
 	}
 }
---
+++ src/main/java/pl/com/project/Users/Person.java
@@ -0,0 +1,50 @@
+package pl.com.project.Users;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+
+    private String surname;
+    private String name;
+
+    public Person(String name, String surname) {
+
+        this.name = name;
+        this.surname=surname;
+    }
+
+    private Person(){}
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}

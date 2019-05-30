1741ea95c21ddf25d813208490f21552edd927bb
Piotr Kwasek <kwasekpiotr98@gmail.com>
2019-05-30 02:03:48 +0200

    Corse!@$%#@

--- src/main/java/pl/example/DemoApplication.java
+++ src/main/java/pl/example/DemoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,6 +24,15 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@EnableWebSecurity
+	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+			http.csrf().disable();
+		}
+	}
 	@Configuration
 	public class MyConfiguration {
 

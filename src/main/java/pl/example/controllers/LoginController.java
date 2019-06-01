0bec8c229c3438676b9251aa1e45860a9ce1a83b
krolik1337 <46852642+krolik1337@users.noreply.github.com>
2019-06-01 13:19:11 +0200

    bd fix

--- src/main/java/pl/example/controllers/LoginController.java
+++ src/main/java/pl/example/controllers/LoginController.java
@@ -1,9 +1,14 @@
 package pl.example.controllers;
 
 
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 public class LoginController {
@@ -13,5 +18,18 @@ public class LoginController {
     public String login()
     {
         return "You've just logged in!";
+    }
+
+    @RequestMapping("/logout")
+    @CrossOrigin(origins = "*")
+    public void exit(HttpServletRequest request, HttpServletResponse response) {
+        // token can be revoked here if needed
+        new SecurityContextLogoutHandler().logout(request, null, null);
+        try {
+            //sending back to client app
+            response.sendRedirect(request.getHeader("referer"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

600842dbd8768044d575bb267d484d3aae51ec1b
Piotr Kwasek <kwasekpiotr98@gmail.com>
2019-06-05 01:38:34 +0200

    test cors

--- src/main/java/pl/example/controllers/OpinionController.java
+++ src/main/java/pl/example/controllers/OpinionController.java
@@ -37,7 +37,7 @@ public class OpinionController {
         opinionService.updateOpinion(id, opinion);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, path="/opinions/{id}")
+    @RequestMapping(method = RequestMethod.OPTIONS, value="/opinions/{id}")
     @CrossOrigin(origins = "*")
     public void deleteOpinion(@PathVariable Integer id) {
         opinionService.deleteOpinion(id);

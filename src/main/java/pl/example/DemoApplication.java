7ba5f10fb1856ff3b9125887f64daa596b369567
krolik1337 <46852642+krolik1337@users.noreply.github.com>
2019-06-05 20:31:02 +0200

    login + adduser

--- src/main/java/pl/example/DemoApplication.java
+++ src/main/java/pl/example/DemoApplication.java
@@ -3,6 +3,7 @@ package pl.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,9 +13,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+import pl.example.configuration.AppProperties;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EnableConfigurationProperties(AppProperties.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {

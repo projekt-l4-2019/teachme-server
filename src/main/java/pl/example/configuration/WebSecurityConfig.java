c434b877e1dbc9413678555301742d7a8723c13d
krolik1337 <46852642+krolik1337@users.noreply.github.com>
2019-06-05 12:23:39 +0200

    login + adduser

--- src/main/java/pl/example/configuration/WebSecurityConfig.java
+++ src/main/java/pl/example/configuration/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -42,13 +43,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/profileedit", "/addnotice", "/contact", "/addopinion", "/login")
+                .antMatchers("/profileedit**", "/addnotice**", "/contact**", "/addopinion**", "/login**")
                 .authenticated()
-                .antMatchers("/*")
+                .antMatchers("/index**")
                 .permitAll()
                 .and()
                 .logout()
-                .logoutSuccessUrl("https://accounts.google.com/o/oauth2/v2/auth/exit");
+                .logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
         http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
         http.csrf().disable();
     }

59cc14f0dfdccb257bc4b1dc9566593cf1f703a8
krolik1337 <46852642+krolik1337@users.noreply.github.com>
2019-06-05 20:13:26 +0200

    login + adduser

--- src/main/java/pl/example/controllers/UserController.java
+++ src/main/java/pl/example/controllers/UserController.java
@@ -14,11 +14,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-//    @RequestMapping("/user")
-//    @CrossOrigin(origins = "*")
-//    public Principal user(Principal user) {
-//        return user;
-//    }
+    @RequestMapping("/user")
+    @CrossOrigin(origins = "*")
+    public Principal user(Principal user) {
+        return user;
+    }
 
     @RequestMapping("/users")
     @CrossOrigin(origins = "*")

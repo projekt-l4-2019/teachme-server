338554dda2312869377ed3e2a91ec457dc3a0df1
krolik1337 <46852642+krolik1337@users.noreply.github.com>
2019-06-05 23:13:48 +0200

    merge

--- src/main/java/pl/example/repository/UserRepository.java
+++ src/main/java/pl/example/repository/UserRepository.java
@@ -1,13 +1,10 @@
 package pl.example.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.example.models.UserrEntity;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserrEntity, Integer> {
     UserrEntity findByEmail(String email);
-    Boolean existsByEmail(String email);
 }
